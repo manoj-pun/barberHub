5b79017d9d1eae90db36c435a808c52529c72322
Manoj Pun <manojpun840@gmail.com>
2025-02-16 14:03:13 +0545

    added

--- users.php
+++ users.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Styled Table</title>
+    <title>Registered Users</title>
     <link rel="stylesheet" href="CSS/admin.css">
 </head>
 
